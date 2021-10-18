23eaa86d7a72e137015904e4e0f3da11413cf1cd
Xchertan01 <Xchertan@outlook.com>
2021-10-18 23:08:20 +0800

    新建内容

--- 特征与标签相关性.py
+++ 特征与标签相关性.py
@@ -1,2 +1,15 @@
 # -*- coding = utf-8 -*-
 # @Software: Pycharm
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Matplotlib中设置字体-黑体，解决Matplotlib中文乱码问题
+plt.rcParams['font.sans-serif'] = ['SimHei']
+# 解决Matplotlib坐标轴负号'-'显示为方块的问题
+plt.rcParams['axes.unicode_minus'] = False
+
+userinfo = pd.read_csv("D:\\Python\\Machine_Learning\\数据\\userinfo_train.csv")
+overdue = pd.read_csv("D:\\Python\\Machine_Learning\\数据\\overdue_train.csv")
